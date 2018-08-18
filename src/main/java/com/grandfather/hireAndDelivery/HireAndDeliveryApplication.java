8f820fae3075fddbc48cd98bffd61ed238abe546
RomanNosachev <Grandfather@MSI>
2018-08-18 22:57:55 +0300

    Created project stub

---
+++ src/main/java/com/grandfather/hireAndDelivery/HireAndDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.grandfather.hireAndDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireAndDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireAndDeliveryApplication.class, args);
+	}
+}
